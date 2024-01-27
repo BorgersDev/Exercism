9e08b90f1e724f0da1826064cef0d631c9f207f5
BorgersDev <154439946+BorgersDev@users.noreply.github.com>
2024-01-27 17:25:16 -0300

    Swift algorithms 5

---
+++ Exercism/swift/custom-signs/Sources/CustomSigns/CustomSigns.swift
@@ -0,0 +1,19 @@
+let birthday = "Birthday"
+let valentine = "Valentine's Day"
+let anniversary = "Anniversary"
+
+let space: Character = " "
+let exclamation: Character = "!"
+
+func buildSign(for occasion: String, name: String) -> String {
+  return  "Happy \(occasion) \(name)!"
+}
+
+
+func graduationFor(name: String, year: Int) -> String {
+  return "Congratulations \(name)!\nClass of \(String(year))"
+}
+
+func costOf(sign: String) -> Int {
+    return 20 + (sign.count * 2)
+}
