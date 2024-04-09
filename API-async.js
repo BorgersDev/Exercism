61b3b96daae42b7ebb8710aefb690d6789c437fb
BorgersDev <154439946+BorgersDev@users.noreply.github.com>
2024-04-09 15:51:06 -0300

    Learning Try and Catch

--- API-async.js
+++ API-async.js
@@ -1,9 +1,15 @@
 const url = 'https://api.chucknorris.io/jokes/random';
 
 async function getData() {
-    const response = await fetch(url);
-    const responseJson = await response.json()
-    console.log(responseJson)
+    try {
+        const response = await fetch(url);
+        const responseJson = await response.json()
+        console.log(responseJson)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        console.log('finally')
+    }
 }
 
 getData()
